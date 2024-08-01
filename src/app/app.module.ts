176bdb974e1ecb9cdf872a5f89618e129c84b717
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-07-31 22:47:37 -0300

    add head e sidemenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// carbon-components-angular default imports
+import { UIShellModule, IconModule, ContainedListModule} from 'carbon-components-angular';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIShellModule,
+    IconModule,
+    ContainedListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
